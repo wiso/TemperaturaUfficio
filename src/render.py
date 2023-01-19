afc3b7680b5e7f58ad64921e2e655041e9510911
Ruggero Turra <ruggero.turra@cern.ch>
2023-01-19 23:28:08 +0100

    format date

--- src/render.py
+++ src/render.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
 
 template = env.get_template("index.html")
 
 today = datetime.datetime.today()
-html = template.render(the="variables", today="%s" % today)
+html = template.render(today="%s" % today.strftime("%d %m %Y"))
 
 with open("index.html", mode="w", encoding="utf-8") as f:
     f.write(html)
+
+import os
+for k, v in os.environ.items():
+    print(k, v)
