2b8bcf8413db99342208f0341554d311e5e3a681
Ruggero Turra <ruggero.turra@cern.ch>
2023-01-19 16:22:00 +0100

    use jinja

---
+++ src/render.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
+
+env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
+
+template = env.get_template("index_template.html")
+
+today = datetime.datetime.today()
+html = template.render(the="variables", today="%s" % today)
+
+with open("index.html", mode="w", encoding="utf-8") as f:
+    f.write(html)
